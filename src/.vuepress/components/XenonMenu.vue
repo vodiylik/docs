513ab8bb3b27370ff8d0c04d3cb7d529ee2b8064
Geno Ferollo <sakhib.orzklv@icloud.com>
2021-01-15 03:36:32 +0500

    xenon menu

--- src/.vuepress/components/XenonMenu.vue
+++ src/.vuepress/components/XenonMenu.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
+  <div class="xenon-option">
     <label>Xenon V2.3.7</label>
   </div>
 </template>
+
+<style>
+ .xenon-option {
+   align-content: center;
+   text-align: center;
+ }
+</style>
