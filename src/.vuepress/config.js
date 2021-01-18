b5e16cbf5345baccfbb07a1dd92c799a663a730c
Geno Ferollo <sakhib.orzklv@icloud.com>
2021-01-18 20:28:36 +0500

    pwa compiled

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -17,24 +17,138 @@ module.exports = {
 	 * ref：https://v1.vuepress.vuejs.org/config/#head
 	 */
 	head: [
-		["link", { rel: "icon", href: "/icons/favicon.ico" }],
-		["link", { rel: "manifest", href: "/manifest.json" }],
-		["meta", { name: "theme-color", content: "#a4a4a4" }],
+		["link", { rel: "icon", href: "/favicon.ico" }],
 		["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
 		[
 			"meta",
 			{ name: "apple-mobile-web-app-status-bar-style", content: "black" },
 		],
-		["link", { rel: "apple-touch-icon", href: "/icons/favicon-152.png" }],
-		["link", { rel: "mask-icon", href: "/xinux.svg", color: "#a4a4a4" }],
+		[
+			"link",
+			{
+				rel: "apple-touch-icon",
+				sizes: "57x57",
+				href: "/apple-icon-57x57.png",
+			},
+		],
+		[
+			"link",
+			{
+				rel: "apple-touch-icon",
+				sizes: "60x60",
+				href: "/apple-icon-60x60.png",
+			},
+		],
+		[
+			"link",
+			{
+				rel: "apple-touch-icon",
+				sizes: "72x72",
+				href: "/apple-icon-72x72.png",
+			},
+		],
+		[
+			"link",
+			{
+				rel: "apple-touch-icon",
+				sizes: "76x76",
+				href: "/apple-icon-76x76.png",
+			},
+		],
+		[
+			"link",
+			{
+				rel: "apple-touch-icon",
+				sizes: "114x114",
+				href: "/apple-icon-114x114.png",
+			},
+		],
+		[
+			"link",
+			{
+				rel: "apple-touch-icon",
+				sizes: "120x120",
+				href: "/apple-icon-120x120.png",
+			},
+		],
+		[
+			"link",
+			{
+				rel: "apple-touch-icon",
+				sizes: "144x144",
+				href: "/apple-icon-144x144.png",
+			},
+		],
+		[
+			"link",
+			{
+				rel: "apple-touch-icon",
+				sizes: "152x152",
+				href: "/apple-icon-152x152.png",
+			},
+		],
+		[
+			"link",
+			{
+				rel: "apple-touch-icon",
+				sizes: "180x180",
+				href: "/apple-icon-180x180.png",
+			},
+		],
+		[
+			"link",
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "192x192",
+				href: "/android-icon-192x192.png",
+			},
+		],
+		[
+			"link",
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "96x96",
+				href: "/favicon-32x32.png",
+			},
+		],
+		[
+			"link",
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "32x32",
+				href: "/favicon-32x32.png",
+			},
+		],
+		[
+			"link",
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "16x16",
+				href: "/favicon-16x16.png",
+			},
+		],
+		["link", { rel: "manifest", href: "/manifest.json" }],
+		["link", { rel: "shortcut icon", href: "/favicon.ico" }],
+		["meta", { name: "msapplication-TileColor", content: "#000000" }],
 		[
 			"meta",
 			{
 				name: "msapplication-TileImage",
-				content: "/icons/favicon-144-precomposed.png",
+				content: "/ms-icon-144x144.png",
 			},
 		],
-		["meta", { name: "msapplication-TileColor", content: "#a4a4a4" }],
+		[
+			"meta",
+			{
+				name: "msapplication-config",
+				content: "/browserconfig.xml",
+			},
+		],
+		["meta", { name: "theme-color", content: "#000000" }],
 	],
 
 	/**
